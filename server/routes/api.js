aa47f58ff3fb42e68616ee1efe0ffb53b724bf38
Jhudiel Bayotlang <mothership@Jhudiels-MacBook-Pro.local>
2018-10-28 01:20:52 +0800

    integrated puppeteer, added validations to api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const axios = require('axios');
+const scrapper = require('../modules/scrapper');
 
 const router = express.Router();
 
@@ -9,14 +9,38 @@ const BING_SEARCH_API_URL = 'https://api.cognitive.microsoft.com/bing/v7.0/searc
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
+
+  if (!req.query.q) {
+    res.status(400);
+    return res.send({
+      status: 'fail',
+      error: 'Please specify a query',
+    });
+  }
+
   const q = `site:stackoverflow.com ${req.query.q}`;
   const offset = req.query.offset || 0;
   const response = await axios.get(`${BING_SEARCH_API_URL}?q=${q}&offset=${offset}`, {
     headers: { 'Ocp-Apim-Subscription-Key': API_KEY },
-  })
+  });
+
   const urls = response.data.webPages.value.map(((v) => v.url));
-  // call SO scraper here
-  res.send(urls);
+
+  try {
+    const data = await scrapper(urls);
+
+    res.status(200);
+    res.send({
+      status: 'success',
+      data,
+    });
+  } catch (e) {
+    res.status(500);
+    res.send({
+      status: 'fail',
+      error: `Internal Server Error: ${e.message}`,
+    });
+  }
 });
 
 module.exports = router;
