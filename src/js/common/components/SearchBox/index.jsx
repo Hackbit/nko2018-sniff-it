7ee6ef3adcb2848610b4b0276bcd585bc6aa6192
Jhudiel Bayotlang <sbayotlang@gmail.com>
2018-10-28 23:39:08 +0800

    Search functionality in homepage

--- src/js/common/components/SearchBox/index.jsx
+++ src/js/common/components/SearchBox/index.jsx
@@ -1,24 +1,51 @@
 import React, { PureComponent, Fragment } from 'react';
-
+import { withRouter } from 'react-router-dom';
 import { GlobalStyle, SearchBoxStyled } from './styles';
 
 class SearchBox extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.searchText = React.createRef();
+    this.redirect = this.redirect.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+  }
+
+  onKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.redirect();
+    }
+  }
+
+  redirect() {
+    const query = this.searchText.current.value;
+    if (query) {
+      this.props.history.push({
+        pathname: '/search',
+        search: `?q=${encodeURIComponent(query.trim())}`,
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.searchText.current.focus();
+  }
+
   render() {
     return (
       <Fragment>
         <SearchBoxStyled>
           <article>&nbsp;</article>
           <section>
-            <input type="text" />
-            <button type="button">Search</button>
+            <input type="text" ref={this.searchText} onKeyPress={this.onKeyPress}/>
+            <button type="button" onClick={this.redirect}>Search</button>
             <p>What problem are you trying to solve today?</p>
           </section>
         </SearchBoxStyled>
         <GlobalStyle />
-        
       </Fragment>
     );
   }
 }
 
-export default SearchBox;
+export default withRouter(SearchBox);
